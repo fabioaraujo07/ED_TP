8937ff7ad92a0cbc3f7fe6217cfcd534b1c08d69
Fabio Araújo <fabiogeovane925@gmail.com>
2024-11-29 00:00:56 +0000

    init commit

---
+++ src/main/java/Classes/Mission.java
@@ -0,0 +1,15 @@
+package Classes;
+import java.util.List;
+
+import Collections.Lists.LinkedUnorderedList;
+import Collections.Lists.UnorderedArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import Collections.Lists.LinkedUnorderedList;
+
+public class Mission {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
