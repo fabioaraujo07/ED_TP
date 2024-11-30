aef3f4a2dc25d1b75f85b2956344760a35952c5e
Fabio Araújo <fabiogeovane925@gmail.com>
2024-11-30 18:29:42 +0000

    Turno ADT

--- src/main/java/Classes/PlayerTurn.java
+++ src/main/java/Classes/PlayerTurn.java
@@ -1,4 +1,20 @@
 package Classes;
 
-public class PlayerTurn {
+import Interfaces.Turn;
+
+public class PlayerTurn<T> extends ToCruz implements Turn<T> {
+
+    public PlayerTurn(String name, Division currentDivision) {
+        super(name, currentDivision);
+    }
+
+    @Override
+    public void atack(T t) {
+
+    }
+
+    @Override
+    public void move(T t, Map map, Division division) {
+
+    }
 }
--- src/main/java/Interfaces/Turn.java
+++ src/main/java/Interfaces/Turn.java
@@ -1,9 +1,12 @@
 package Interfaces;
 
+import Classes.Division;
+import Classes.Map;
+
 public interface Turn<T>{
 
     public void atack(T t);
 
-    public void move(T t);
+    public void move(T t, Map map, Division division);
 
 }
